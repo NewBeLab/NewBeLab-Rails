9bb8d063f5176c01ff50c3a26281888103f7c2ef
eityamo <eityamo@gmail.com>
2022-11-17 22:21:41 +0900

    add: deviceの導入, device_token_authを導入

--- Gemfile
+++ Gemfile
@@ -46,6 +46,9 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+# 認証
+gem 'devise_token_auth', '~> 1.1', '>= 1.1.5'
+
 gem 'net-smtp'
 gem 'net-imap'
 gem 'net-pop'
---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer          not null, primary key
+#  email              :string
+#  encrypted_password :string           default(""), not null
+#  name               :string
+#  provider           :string           default("email"), not null
+#  tokens             :json
+#  uid                :string           default(""), not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email             (email) UNIQUE
+#  index_users_on_uid_and_provider  (uid,provider) UNIQUE
+#
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+end
