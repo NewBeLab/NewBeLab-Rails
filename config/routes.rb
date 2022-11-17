af1e4e97847422ff0f44d2fefe778e90c5d279df
eityamo <eityamo@gmail.com>
2022-11-17 23:11:35 +0900

    add: CORS対策

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', skip: [:password], controllers: {
+        registrations: 'api/v1/auth/registrations',
+        omniauth_callbacks: 'api/v1/auth/omniauth_callbacks'
+      }
+    end
+  end
 end
