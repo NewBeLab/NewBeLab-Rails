f1ec330ed6ee36f694a477f5d78780b04b6e791c
eityamo <eityamo@gmail.com>
2022-11-19 15:22:31 +0900

    add: セッションとクッキーの設定

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,9 @@ module NewBeLabRails
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.session_store :cookie_store, key: '_session_mechaco'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
     # csrf対策を無効化する設定
     config.action_controller.allow_forgery_protection = false
   end
