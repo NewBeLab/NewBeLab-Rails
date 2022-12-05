36070a54cdc6ffcc2dd522f75107172f17d42424
eityamo <eityamo@gmail.com>
2022-12-05 21:23:53 +0900

    add:Cookieの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :skip_session
+
+  protected
+
+  def skip_session
+    request.session_options[:skip] = true
+  end
 end
