07400a88777837588cc89b57cd8c8a8b99a1932d
isakashiori <isslamdunk0000@gmail.com>
2022-12-11 15:13:49 +0900

    add: 中間テーブル＆アソシエーションの追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: tags
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Tag < ApplicationRecord
+  has_many :user_tags, dependent: :destroy
+  has_many :users, through: :user_tags
+
+  validates :name, uniqueness: true
 end
