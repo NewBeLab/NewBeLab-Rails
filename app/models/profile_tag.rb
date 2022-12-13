1e2acc8ef82410d76b0b3bf3484a9c5ed6034fff
isakashiori <isslamdunk0000@gmail.com>
2022-12-13 19:26:56 +0900

    fix: db:migrateを実行でテーブル生成

--- app/models/profile_tag.rb
+++ app/models/profile_tag.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: profile_tags
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  profile_id :integer          not null
+#  tag_id     :integer          not null
+#
+# Indexes
+#
+#  index_profile_tags_on_profile_id  (profile_id)
+#  index_profile_tags_on_tag_id      (tag_id)
+#
+# Foreign Keys
+#
+#  profile_id  (profile_id => profiles.id)
+#  tag_id      (tag_id => tags.id)
+#
 class ProfileTag < ApplicationRecord
   belongs_to :profile
   belongs_to :tag
