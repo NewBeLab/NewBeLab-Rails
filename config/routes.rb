a8d7452bc4ef785e4f1b343ad7046aace1c10934
isakashiori <isslamdunk0000@gmail.com>
2023-01-16 16:41:53 +0900

    add: ransackにて各項目の検索設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         omniauth_callbacks: 'api/v1/auth/omniauth_callbacks'
       }
-      resources :profiles, only: %i[index show update], params: :user_id
+      resources :profiles, only: %i[index show update], params: :user_id 
       resources :tags, only: :index
       namespace :auth do
         resources :sessions, only: %i[index]
