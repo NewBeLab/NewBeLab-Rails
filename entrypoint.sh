c0d1c9b028437e07315c1907d48d6bcac2ff3e31
fujiwara yuji <62694188+nyohoo@users.noreply.github.com>
2023-01-19 21:05:16 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@ set -e
 
 # 自分のアプリに合わせて必要なコマンドを修正してください
 
-RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rails db:migrate:reset
-# bin/rails db:migrate:reset
+bin/rails db:migrate
 # bin/rails db:seed
 
 rm -f tmp/pids/server.pid && bin/rails s
